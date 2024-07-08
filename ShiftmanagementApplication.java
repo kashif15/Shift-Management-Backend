5529d094603d83e46f292b46d0092619bb51b70f
kashif15 <60213456+kashif15@users.noreply.github.com>
2024-07-08 13:04:43 +0530

    Add files via upload

---
+++ ShiftmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.shiftmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiftmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShiftmanagementApplication.class, args);
+	}
+
+}
